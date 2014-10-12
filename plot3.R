28690d5673674f0a13eadefcf10ead1dfc86a465
fuentes81081 <fuentes81081@hotmail.com>
2014-10-12 10:30:56 -0700

    Adding legend

--- plot3.R
+++ plot3.R
@@ -31,4 +31,13 @@ lines(workingData$DateTime, workingData$Sub_metering_1, col="black")
 lines(workingData$DateTime, workingData$Sub_metering_2, col="red")
 lines(workingData$DateTime, workingData$Sub_metering_3, col="blue")
 
+legend(
+    "topright",
+    ,
+    c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+    lty=c(1,1,1),
+    col=c("black", "red", "blue")
+)
+
+
 dev.off()
